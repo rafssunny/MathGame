b356e0635a8c95d3a0fe350a64f8df2a75203f85
Rafael Olegário <rafaelolegario2008@gmail.com>
2025-11-22 10:13:47 -0300

    melhorando nomenclatura das variaveis

--- game.js
+++ game.js
@@ -1,96 +1,110 @@
 /*Lógica do jogo*/
 /* vars */
-const Timer_DOM = document.getElementById('timer')
+const timer_dom = document.getElementById('timer')
 let seconds
 
-let Pontos = 0
-const Pontos_DOM = document.getElementById('pontos')
+let pontos = 0
+const pontos_dom = document.getElementById('pontos')
 
-const TelaDeDerrota_DOM = document.querySelector('.loser')
-const PontosFinais_DOM = document.getElementById('final_points')
+const tela_de_derrota_dom = document.querySelector('.loser')
+const pontos_finais_dom = document.getElementById('final_points')
 
-const TelaDeVitoria_DOM = document.querySelector('.win')
+const tela_de_vitoria_dom = document.querySelector('.win')
 
 /* funcoes de expressao numerica*/
 function GerarValores(){
-    let NumerosSelecionados = []
+    let numeros_selecionados = []
 
     /* +1 pra evitar q o valor seja igual a 0*/
-    NumeroParaSubtracao_maior = Math.floor(Math.random() * 101) + 1
-    NumeroParaSubtracao_menor = Math.floor(Math.random() * 91) + 1
-    while(NumeroParaSubtracao_menor > NumeroParaSubtracao_maior){
-        NumeroParaSubtracao_menor = Math.floor(Math.random()*(NumeroParaSubtracao_maior)) + 1
+    numero_para_subtracao_maior = Math.floor(Math.random() * 101) + 1
+    numero_para_subtracao_menor = Math.floor(Math.random() * 91) + 1
+    while(numero_para_subtracao_menor > numero_para_subtracao_maior){
+        numero_para_subtracao_menor = Math.floor(Math.random() * (numero_para_subtracao_maior)) + 1
     }
 
-    NumeroParaMultiplicacao = Math.floor(Math.random() * 11) + 1
+    numero_para_multiplicacao = Math.floor(Math.random() * 11) + 1
 
-    NumeroParaRaizQuadrada = Math.floor(Math.random() * 401) + 1
-    while(Math.sqrt(NumeroParaRaizQuadrada) % 1 != 0){
-        NumeroParaRaizQuadrada = Math.floor(Math.random() * 401) + 1
+    numero_para_raiz_quadrada = Math.floor(Math.random() * 401) + 1
+    while(Math.sqrt(numero_para_raiz_quadrada) % 1 != 0){
+        numero_para_raiz_quadrada = Math.floor(Math.random() * 401) + 1
     }
 
-    NumeroParaPotencia = Math.floor(Math.random() * 9) + 1 
+    numero_para_potencia = Math.floor(Math.random() * 9) + 1 
 
-    NumeroParaDivisao_numerador = Math.floor(Math.random() * 101) + 1
-    NumeroParaDivisao_denominador = Math.floor(Math.random() * 201) + 1
-    while(NumeroParaDivisao_numerador % NumeroParaDivisao_denominador != 0){
-        NumeroParaDivisao_numerador = Math.floor(Math.random() * 201) + 1
-        NumeroParaDivisao_denominador = Math.floor(Math.random() * 101) + 1
+    numero_para_divisao_numerador = Math.floor(Math.random() * 101) + 1
+    numero_para_divisao_denominador = Math.floor(Math.random() * 201) + 1
+    while(numero_para_divisao_numerador % numero_para_divisao_denominador != 0){
+        numero_para_divisao_numerador = Math.floor(Math.random() * 201) + 1
+        numero_para_divisao_denominador = Math.floor(Math.random() * 101) + 1
     } 
 
-    NumerosSelecionados.push(NumeroParaSubtracao_maior, NumeroParaSubtracao_menor, NumeroParaMultiplicacao, NumeroParaRaizQuadrada, NumeroParaPotencia, NumeroParaDivisao_numerador, NumeroParaDivisao_denominador)
-    return NumerosSelecionados
-
+    numeros_selecionados.push(
+        numero_para_subtracao_maior,
+        numero_para_subtracao_menor,
+        numero_para_multiplicacao,
+        numero_para_raiz_quadrada,
+        numero_para_potencia,
+        numero_para_divisao_numerador,
+        numero_para_divisao_denominador
+    )
+    return numeros_selecionados
 }
+
 function GerarExpressoesNumericas(){
     let [n1, n2, n3, n4, n5, n6, n7] = GerarValores()
-    let NumeroAleatorio = Math.floor(Math.random() * 8)
-    switch(NumeroAleatorio){
+    let numero_aleatorio = Math.floor(Math.random() * 8)
+
+    switch(numero_aleatorio){
         case 0:
-            ResultadoDaExpressao = n1-n2
-            return [`${n1} - ${n2}`, ResultadoDaExpressao]
+            resultado_da_expressao = n1 - n2
+            return [`${n1} - ${n2}`, resultado_da_expressao]
         case 1: 
-            ResultadoDaExpressao = n1*n3
-            return [`${n1} x ${n3}`, ResultadoDaExpressao]
+            resultado_da_expressao = n1 * n3
+            return [`${n1} x ${n3}`, resultado_da_expressao]
         case 2:
-            ResultadoDaExpressao = Math.sqrt(n4) + n1
-            return [`√${n4} + ${n1}`, ResultadoDaExpressao]
+            resultado_da_expressao = Math.sqrt(n4) + n1
+            return [`√${n4} + ${n1}`, resultado_da_expressao]
         case 3:
-            ResultadoDaExpressao = Math.sqrt(n4) + (n5*n5)
-            return [`√${n4} + ${n5}²`, ResultadoDaExpressao]
+            resultado_da_expressao = Math.sqrt(n4) + (n5 * n5)
+            return [`√${n4} + ${n5}²`, resultado_da_expressao]
         case 4:
-            ResultadoDaExpressao = n6/n7
-            return[`${n6} ÷ ${n7}`, ResultadoDaExpressao]
+            resultado_da_expressao = n6 / n7
+            return [`${n6} ÷ ${n7}`, resultado_da_expressao]
         case 5:
-            ResultadoDaExpressao = ((n5*n5) + (n6/n7))
-            return[`${n5}² + (${n6} ÷ ${n7})`, ResultadoDaExpressao]
+            resultado_da_expressao = ((n5 * n5) + (n6 / n7))
+            return [`${n5}² + (${n6} ÷ ${n7})`, resultado_da_expressao]
         case 6:
-            ResultadoDaExpressao = n3*((n5*n5)+Math.sqrt(n4))
-            return[`${n3} x (${n5}² + √${n4})`, ResultadoDaExpressao]    
+            resultado_da_expressao = n3 * ((n5 * n5) + Math.sqrt(n4))
+            return [`${n3} x (${n5}² + √${n4})`, resultado_da_expressao]    
         default:
-            ResultadoDaExpressao = (n6/n7)*(n5*n5)
-            return[`(${n6} ÷ ${n7}) x ${n5}²`, ResultadoDaExpressao]
+            resultado_da_expressao = (n6/n7) * (n5*n5)
+            return [`(${n6} ÷ ${n7}) x ${n5}²`, resultado_da_expressao]
     }
 }
+
 function MostrarExpressaoNaTela(){
-    const ExpressaoNumerica_DOM = document.getElementById('calc')
-    let[ExpressaoNumerica, ResultadoDaExpressao] = GerarExpressoesNumericas()
-    ExpressaoNumerica_DOM.textContent = ExpressaoNumerica
-    return ResultadoDaExpressao
+    const expressao_numerica_dom = document.getElementById('calc')
+    let [expressao_numerica, resultado_da_expressao] = GerarExpressoesNumericas()
+    expressao_numerica_dom.textContent = expressao_numerica
+    return resultado_da_expressao
 }
+
 
 /* funcoes de timer*/
 let interval_id 
+
 function UpdateTime(){
     seconds--
-    Timer_DOM.textContent = (`⏱️${seconds}s`)
+    timer_dom.textContent = (`⏱️${seconds}s`)
+
     if (seconds == 0){
         FinalizarJogoComoDerrota()
     }
 }
+
 function StartTime(){
     seconds = 16
-    Timer_DOM.textContent = (`⏱️${seconds}s`)
+    timer_dom.textContent = (`⏱️${seconds}s`)
     interval_id = setInterval(UpdateTime, 1000)
     UpdateTime()
 }
@@ -99,16 +113,18 @@ function StopTime(){
     clearInterval(interval_id)
 }
 
+
 /* checar se é vitoria ou derrota*/
 function ChecarResposta(){
     input_resposta = input.value.trim()
-    if (input_resposta.trim() == ResultadoDaExpressao){
-        Pontos++
-        const PontuacaoParaGanhar = 25
-        if (Pontos == PontuacaoParaGanhar){
+
+    if (input_resposta.trim() == resultado_da_expressao){
+        pontos++
+        const pontuacao_para_ganhar = 25
+
+        if (pontos == pontuacao_para_ganhar){
             FinalizarJogoComoVitoria()
-        }
-        else{
+        } else {
             GerarNovaExpressao()
         }
     }
@@ -116,16 +132,18 @@ function ChecarResposta(){
     else{
         FinalizarJogoComoDerrota()
     }
-    }
+}
+
 /* caso de vitoria*/
 function FinalizarJogoComoVitoria(){
-    TelaDeVitoria_DOM.style.display = 'block'
+    tela_de_vitoria_dom.style.display = 'block'
     input.blur()
     StopTime()
 }
+
 function GerarNovaExpressao(){
-    ResultadoDaExpressao = MostrarExpressaoNaTela()
-    Pontos_DOM.textContent = ('Pontos: ' + Pontos)
+    resultado_da_expressao = MostrarExpressaoNaTela()
+    pontos_dom.textContent = ('Pontos: ' + pontos)
     input.value = ''
     clearInterval(interval_id)
     StartTime()
@@ -137,16 +155,16 @@ function FinalizarJogoComoDerrota(){
     StopTime()
     MostrarTelaDeDerrota()
 }
+
 function MostrarTelaDeDerrota(){
-    PontosFinais_DOM.textContent = `Sua pontuação: ${Pontos}`
-    TelaDeDerrota_DOM.style.display = 'block'
+    pontos_finais_dom.textContent = `Sua pontuação: ${pontos}`
+    tela_de_derrota_dom.style.display = 'block'
     input.blur()
 }
 
 /* iniciar*/
-ResultadoDaExpressao = MostrarExpressaoNaTela()
+resultado_da_expressao = MostrarExpressaoNaTela()
 StartTime()
-
 
 /* logica de pegar entrada */
 const input = document.getElementById('resposta')
@@ -155,5 +173,4 @@ input.addEventListener('keydown', function(e){
     if(e.code === 'Enter'){
         ChecarResposta()
     }
-    }
-)
+})
